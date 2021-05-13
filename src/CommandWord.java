00aff86e89d832a10d01af984615c686b42b17b3
Floovera <r0385588@student.thomasmore.be>
2021-05-13 21:48:45 +0200

    Hashmap toegevoegd met commands Supermarkt

---
+++ src/CommandWord.java
@@ -0,0 +1,4 @@
+public enum CommandWord {
+        GO, LOOK, TAKE, DROP, EAT, QUIT, HELP, UNKNOWN
+}
+
--- src/CommandWords.java
+++ src/CommandWords.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * This class is part of the "World of Zuul" application. 
  * "World of Zuul" is a very simple, text based adventure game.  
@@ -12,16 +14,17 @@
 public class CommandWords
 {
     // a constant array that holds all valid command words
-    private static final String[] validCommands = {
-        "go", "quit", "help","look","take","drop"
-    };
+    private HashMap<String, CommandWord> validCommands;
 
     /**
      * Constructor - initialise the command words.
      */
     public CommandWords()
     {
-        // nothing to do at the moment...
+        validCommands = new HashMap<String, CommandWord>();
+        validCommands.put("go", CommandWord.GO);
+        validCommands.put("help", CommandWord.HELP);
+        validCommands.put("quit", CommandWord.QUIT);
     }
 
     /**
@@ -29,21 +32,28 @@ public class CommandWords
      * @return true if a given string is a valid command,
      * false if it isn't.
      */
-    public boolean isCommand(String aString)
-    {
-        for(int i = 0; i < validCommands.length; i++) {
-            if(validCommands[i].equals(aString))
-                return true;
+    public boolean isCommand(String aString) {
+        if (validCommands.containsKey(aString)) {
+            return true;
+        } else {
+            // if we get here, the string was not found in the commands
+            return false;
         }
-        // if we get here, the string was not found in the commands
-        return false;
     }
 
     public String showAll() {
         String show = "";
-        for (String command : validCommands) {
+        for (String command : validCommands.keySet()) {
             show += command + " ";
         }
         return show;
+    }
+
+    public CommandWord getCommand(String aString) {
+        if (validCommands.containsKey(aString)) {
+            return validCommands.get(aString);
+        }
+        else{
+        return CommandWord.UNKNOWN;}
     }
 }
